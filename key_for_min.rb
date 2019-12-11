73f437ab8370863aaea1d316d7aa0fcd23c2d0fa
melia miller <meliajm@gmail.com>
2019-12-11 05:01:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,13 +5,13 @@ require 'pry'
 def key_for_min_value(name_hash)
   # binding.pry 
   # min_value = 0 
-  array = []
-  name_hash.each { |key, value|
-    min_value = value
-    if value < min_value
-      min_value = value
-    end 
-  min_value ? min_value : nil 
+  # array = []
+  # name_hash.each { |key, value|
+  #   min_value = value
+  #   if value < min_value
+  #     min_value = value
+  #   end 
+  # min_value ? min_value : nil 
     
-  }
+  # }
 end
